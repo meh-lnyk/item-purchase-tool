79471326e125d4e2c768fb517ea5192f6a2dabe7
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-06-19 00:01:46 +0300

    added basic LWC with initial layout and data load

---
+++ force-app/main/default/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -0,0 +1,16 @@
+import { LightningElement, track } from 'lwc';
+import getItems from '@salesforce/apex/ItemService.getItems';
+
+export default class ItemPurchaseTool extends LightningElement {
+    @track items;
+
+    connectedCallback() {
+        getItems()
+            .then(result => {
+                this.items = result;
+            })
+            .catch(error => {
+                console.error('Error loading items:', error);
+            });
+    }
+}
