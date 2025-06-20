d62775ffa45cbf35c8bdc9efdba6af939edd2b1b
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-06-20 19:22:09 +0300

    added initial item card display

--- force-app/main/default/lwc/itemPurchaseTool/itemPurchaseTool.js
+++ force-app/main/default/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -2,7 +2,7 @@ import { LightningElement, track } from 'lwc';
 import getItems from '@salesforce/apex/ItemService.getItems';
 
 export default class ItemPurchaseTool extends LightningElement {
-    @track items;
+    @track items = [];
 
     connectedCallback() {
         getItems()
@@ -12,5 +12,9 @@ export default class ItemPurchaseTool extends LightningElement {
             .catch(error => {
                 console.error('Error loading items:', error);
             });
+    }
+
+    handleAddToCart(event) {
+        console.log('Add to cart clicked');
     }
 }
